a23d0d512f0f7b48dfda223219e86e7967c364a9
ElPsy <43912638+JhonnySalles@users.noreply.github.com>
2024-04-28 22:03:07 -0300

    Ajuste para atualização da informaçao de legendas.

--- BilingualReader/src/main/java/br/com/fenix/bilingualreader/service/repository/DataBaseDAO.kt
+++ BilingualReader/src/main/java/br/com/fenix/bilingualreader/service/repository/DataBaseDAO.kt
@@ -93,9 +93,6 @@ abstract class MangaDAO : DataBaseDAO<Manga> {
     @Query("UPDATE " + DataBaseConsts.MANGA.TABLE_NAME + " SET " + DataBaseConsts.MANGA.COLUMNS.BOOK_MARK + " = :marker " + " WHERE " + DataBaseConsts.MANGA.COLUMNS.ID + " = :id ")
     abstract fun updateBookMark(id: Long, marker: Int)
 
-    @Query("UPDATE " + DataBaseConsts.MANGA.TABLE_NAME + " SET " + DataBaseConsts.MANGA.COLUMNS.HAS_SUBTITLE + " = :hasSubtitle, " + DataBaseConsts.MANGA.COLUMNS.LAST_VOCABULARY_IMPORT + " = null WHERE " + DataBaseConsts.MANGA.COLUMNS.ID + " = :idManga")
-    abstract fun updateHasSubtitle(idManga: Long, hasSubtitle: Boolean)
-
     @Query("UPDATE " + DataBaseConsts.MANGA.TABLE_NAME + " SET " + DataBaseConsts.MANGA.COLUMNS.EXCLUDED + " = 1 WHERE " + DataBaseConsts.MANGA.COLUMNS.ID + " = :id")
     abstract fun delete(id: Long)
 
@@ -183,8 +180,9 @@ abstract class SubTitleDAO : DataBaseDAO<SubTitle> {
     @Query("DELETE FROM " + DataBaseConsts.SUBTITLES.TABLE_NAME + " WHERE " + DataBaseConsts.SUBTITLES.COLUMNS.FK_ID_MANGA + " = :idManga")
     abstract fun deleteAll(idManga: Long)
 
-    @Query("UPDATE " + DataBaseConsts.MANGA.TABLE_NAME + " SET " + DataBaseConsts.MANGA.COLUMNS.HAS_SUBTITLE + " = :hasSubtitle, " + DataBaseConsts.MANGA.COLUMNS.LAST_VOCABULARY_IMPORT + " = null WHERE " + DataBaseConsts.MANGA.COLUMNS.ID + " = :idManga")
-    abstract fun updateHasSubtitle(idManga: Long, hasSubtitle: Boolean)
+    @Query("UPDATE " + DataBaseConsts.MANGA.TABLE_NAME + " SET " + DataBaseConsts.MANGA.COLUMNS.HAS_SUBTITLE + " = :hasSubtitle, " + DataBaseConsts.MANGA.COLUMNS.LAST_ALTERATION + " = :lastAlteration, " +
+            DataBaseConsts.MANGA.COLUMNS.LAST_VOCABULARY_IMPORT + " = null WHERE " + DataBaseConsts.MANGA.COLUMNS.ID + " = :idManga")
+    abstract fun updateHasSubtitle(idManga: Long, hasSubtitle: Boolean, lastAlteration : LocalDateTime = LocalDateTime.now())
 
 }
 
