9d6efbd4d4e88303039a044498662081b6239682
ElPsy <43912638+JhonnySalles@users.noreply.github.com>
2024-05-05 07:33:13 -0300

    Apresentação de mensagem.

--- BilingualReader/src/main/java/br/com/fenix/bilingualreader/view/ui/library/book/BookLibraryFragment.kt
+++ BilingualReader/src/main/java/br/com/fenix/bilingualreader/view/ui/library/book/BookLibraryFragment.kt
@@ -987,6 +987,8 @@ class BookLibraryFragment : Fragment(), PopupOrderListener, SwipeRefreshLayout.O
 
                     if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED)
                         notificationManager.notify(notifyId, notification.build())
+
+                    Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
                 }
             }
         }
