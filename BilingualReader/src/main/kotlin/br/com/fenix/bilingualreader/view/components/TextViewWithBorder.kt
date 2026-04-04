aab624fc8c18db8958ab12ba1e018ae4c19bb9c6
ElPsy <43912638+JhonnySalles@users.noreply.github.com>
2026-04-04 14:59:16 -0300

    Correção de loop infinito no desenho do texto do card.

--- BilingualReader/src/main/kotlin/br/com/fenix/bilingualreader/view/components/TextViewWithBorder.kt
+++ BilingualReader/src/main/kotlin/br/com/fenix/bilingualreader/view/components/TextViewWithBorder.kt
@@ -4,9 +4,9 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.text.TextPaint
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatTextView
-
 
 class TextViewWithBorder : AppCompatTextView {
     constructor(context: Context) : super(context)
@@ -15,15 +15,30 @@ class TextViewWithBorder : AppCompatTextView {
 
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
+    private val borderPaint = TextPaint()
+
+    init {
+        borderPaint.isAntiAlias = true
+        borderPaint.style = Paint.Style.STROKE
+        borderPaint.strokeWidth = 1f
+        borderPaint.color = Color.BLACK
+        borderPaint.strokeJoin = Paint.Join.ROUND
+        borderPaint.strokeMiter = 10f
+    }
+
     public override fun onDraw(canvas: Canvas) {
-        val textColor = textColors.defaultColor
-        setTextColor(Color.BLACK)
-        paint.strokeWidth = 2f
-        paint.style = Paint.Style.STROKE
-        super.onDraw(canvas)
-        setTextColor(textColor)
-        paint.strokeWidth = 0f
-        paint.style = Paint.Style.FILL
+        val text = text
+        if (text == null || text.isEmpty()) {
+            super.onDraw(canvas)
+            return
+        }
+
+        borderPaint.typeface = paint.typeface
+        borderPaint.textSize = paint.textSize
+        borderPaint.letterSpacing = paint.letterSpacing
+        val x = compoundPaddingLeft.toFloat()
+        val y = (height / 2f + borderPaint.textSize / 3f) + (totalPaddingTop - totalPaddingBottom) / 2f
+        canvas.drawText(text.toString(), x, y, borderPaint)
         super.onDraw(canvas)
     }
 }
