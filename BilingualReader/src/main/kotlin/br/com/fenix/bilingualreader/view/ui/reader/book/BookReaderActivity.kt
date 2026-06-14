5c0eab0bb7faffd98638d6a15bc4435627684c3b
ElPsy <43912638+JhonnySalles@users.noreply.github.com>
2026-06-14 12:03:49 -0300

    Modernização do touch de demonstração.

--- BilingualReader/src/main/kotlin/br/com/fenix/bilingualreader/view/ui/reader/book/BookReaderActivity.kt
+++ BilingualReader/src/main/kotlin/br/com/fenix/bilingualreader/view/ui/reader/book/BookReaderActivity.kt
@@ -644,7 +644,7 @@ class BookReaderActivity : AppCompatActivity(), PopupLayoutListener {
                 ContextCompat.getColor(this, R.color.touch_demonstration_alter)
             }
 
-            val alpha = if (isGlass) 0x66 else 0x26 // 40% for Glass, 15% (85% transparency) for Flat
+            val alpha = if (isGlass) 0x66 else 0xA6 // 40% for Glass, 65% (35% transparency) for Flat
             val dynamicColor = (baseColor and 0x00FFFFFF) or (alpha shl 24)
 
             val cornerRadius = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16f, resources.displayMetrics)
