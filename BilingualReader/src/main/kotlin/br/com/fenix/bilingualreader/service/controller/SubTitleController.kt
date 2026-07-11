951c1892f594bee8dddf4498187d665277894151
ElPsy <43912638+JhonnySalles@users.noreply.github.com>
2026-07-11 10:51:53 -0300

    Correção de erro na compilação.

--- BilingualReader/src/main/kotlin/br/com/fenix/bilingualreader/service/controller/SubTitleController.kt
+++ BilingualReader/src/main/kotlin/br/com/fenix/bilingualreader/service/controller/SubTitleController.kt
@@ -100,8 +100,8 @@ class SubTitleController private constructor(private val context: Context) {
     private var mForceExpandFloatingPopup: MutableLiveData<Boolean> = MutableLiveData(true)
     var forceExpandFloatingPopup: LiveData<Boolean> = mForceExpandFloatingPopup
 
-    private var mSelectedSubTitle: MutableLiveData<SubTitle> = MutableLiveData()
-    var selectedSubtitle: LiveData<SubTitle> = mSelectedSubTitle
+    private var mSelectedSubTitle: MutableLiveData<SubTitle?> = MutableLiveData()
+    var selectedSubtitle: LiveData<SubTitle?> = mSelectedSubTitle
 
     private lateinit var mSubtitleLang: Languages
     private lateinit var mTranslateLang: Languages
@@ -648,16 +648,17 @@ class SubTitleController private constructor(private val context: Context) {
                     }
             }
 
-            if (mSelectedSubTitle.value?.pageCount != pageNumber) {
-                var differ = pageNumber - mSelectedSubTitle.value?.pageCount!!
+            val pageCount = mSelectedSubTitle.value?.pageCount ?: 0
+            if (pageCount != pageNumber) {
+                var differ = pageNumber - pageCount
                 if (differ == 0) differ = 1
-                val run = if (mSelectedSubTitle.value?.pageCount!! < pageNumber)
+                val run = if (pageCount < pageNumber)
                     getNextSelectPage(differ)
                 else
                     getBeforeSelectPage(false, differ * -1)
 
                 if (!run) {
-                    if (mSelectedSubTitle.value?.pageCount!! < pageNumber)
+                    if (pageCount < pageNumber)
                         getNextSelectSubtitle()
                     else
                         getBeforeSelectSubtitle()
@@ -854,7 +855,8 @@ class SubTitleController private constructor(private val context: Context) {
     }
 
     private fun getNextSelectSubtitle(): Boolean {
-        val index: Int = if (mChaptersKeys.value!!.isNotEmpty()) mChaptersKeys.value!!.indexOf(mSelectedSubTitle.value?.chapterKey!!).plus(1) else 0
+        val chapterKey = mSelectedSubTitle.value?.chapterKey ?: return false
+        val index: Int = if (mChaptersKeys.value!!.isNotEmpty()) mChaptersKeys.value!!.indexOf(chapterKey).plus(1) else 0
 
         return if (getSubtitle().keys.size >= index && getSubtitle().containsKey(mChaptersKeys.value!![index])) {
             setChapter(getSubtitle()[mChaptersKeys.value!![index]])
@@ -864,7 +866,8 @@ class SubTitleController private constructor(private val context: Context) {
     }
 
     private fun getBeforeSelectSubtitle(): Boolean {
-        val index: Int = if (mChaptersKeys.value!!.isNotEmpty()) mChaptersKeys.value!!.indexOf(mSelectedSubTitle.value?.chapterKey!!).minus(1) else 0
+        val chapterKey = mSelectedSubTitle.value?.chapterKey ?: return false
+        val index: Int = if (mChaptersKeys.value!!.isNotEmpty()) mChaptersKeys.value!!.indexOf(chapterKey).minus(1) else 0
 
         return if (index >= 0 && getSubtitle().containsKey(mChaptersKeys.value!![index])) {
             setChapter(getSubtitle()[mChaptersKeys.value!![index]])
@@ -919,8 +922,9 @@ class SubTitleController private constructor(private val context: Context) {
         if (subTitleChapterSelected.value == null)
             return true
 
-        val index: Int = if (mSelectedSubTitle.value?.pageKey!!.isNotEmpty())
-            mPagesKeys.value!!.indexOf(mSelectedSubTitle.value?.pageKey!!).plus(differ)
+        val pageKey = mSelectedSubTitle.value?.pageKey ?: ""
+        val index: Int = if (pageKey.isNotEmpty())
+            mPagesKeys.value!!.indexOf(pageKey).plus(differ)
         else
             0
 
@@ -935,10 +939,11 @@ class SubTitleController private constructor(private val context: Context) {
         if (subTitleChapterSelected.value == null)
             return true
 
-        val index: Int =
-            if (mSelectedSubTitle.value?.pageKey!!.isNotEmpty()) mPagesKeys.value!!.indexOf(
-                mSelectedSubTitle.value?.pageKey!!
-            ).minus(differ) else 0
+        val pageKey = mSelectedSubTitle.value?.pageKey ?: ""
+        val index: Int = if (pageKey.isNotEmpty())
+            mPagesKeys.value!!.indexOf(pageKey).minus(differ)
+        else
+            0
 
         return if (index >= 0 && mListPages.containsKey(mPagesKeys.value!![index])) {
             setPage(lastText, mListPages[mPagesKeys.value!![index]])
